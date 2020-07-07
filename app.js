2384eec5197c1ee27e09aedc5672b27b0ec78be5
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-06 23:23:37 -0300

    "Commit inicial do projeto"

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { studentRouter } from './routes/studentRouter.js';
+import mongoose from 'mongoose';
+
+(async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://user-bootcamp:230992@bootcamp.csvsk.mongodb.net/grades?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (error) {
+    console.log('Ocorreu um erro' + error);
+  }
+})();
+
+const app = express();
+
+app.use(express.json());
+app.use(studentRouter);
+
+app.listen(3000, () => {
+  console.log('API iniciada');
+});
