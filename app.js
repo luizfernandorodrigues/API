212f8077ecb72b0b45e335b61329c45f99c782c3
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-07 23:07:11 -0300

    AJUSTE STRING DE CONEXÃO

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ import { studentRouter } from './routes/studentRouter.js';
 import mongoose from 'mongoose';
 
 console.log(process.env.USER_DB);
+console.log(process.env.PASSWORD);
+console.log(process.env.PORT);
 //conexão com o mongodb
 (async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.USER_DB}
-        :
-        ${process.env.PASSWORD}
-        @bootcamp.csvsk.mongodb.net/grades?retryWrites=true&w=majority`,
+      'mongodb+srv://' +
+        process.env.USER_DB +
+        ':' +
+        process.env.PASSWORD +
+        '@bootcamp.csvsk.mongodb.net/grades?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
